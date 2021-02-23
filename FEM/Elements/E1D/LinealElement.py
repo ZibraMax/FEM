3b7221452fd0c44d7627b4757822fc3e13573d7d
Arturo Rodriguez <da.rodriguezh@uniandes.edu.co>
2021-02-22 23:45:13 -0500

    GiD para enmallado funciona genial, arreglado lo de matrices y deformaciones. Ahora dan las correctas. Problemas con solucion en 1D por extraña asíntota. Posiblemetne sea el enmallado. Feliz porque funciona.

--- FEM/Elements/E1D/LinealElement.py
+++ FEM/Elements/E1D/LinealElement.py
@@ -3,6 +3,7 @@ class LinealElement(Element1D):
 	def __init__(this,coords,gdl,n=2):
 		Element1D.__init__(this,coords,gdl,n)
 	def psis(this,z):
-		return np.array([0.5*(1.0-z),0.5*(1.0+z)])
+		return np.array([0.5*(1.0-z),0.5*(1.0+z)]).T
 	def dpsis(this,z):
-		return np.array([[-0.5,0.5]])
+		kernel = 1+(z-z)
+		return np.array([[-0.5*kernel],[0.5*kernel]])
