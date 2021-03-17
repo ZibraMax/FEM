af143ba61067fa4a58c5e9a7dd36665f7acdd157
Arturo Rodriguez <da.rodriguezh@uniandes.edu.co>
2021-03-17 18:59:22 -0500

    Fortran support on vsc

--- Fortran_tests/test.f90
+++ Fortran_tests/test.f90
@@ -1,35 +1,23 @@
-! -*- f90 -*-
-subroutine sieve(is_prime, n_max)
-! =====================================================
-! Uses the sieve of Eratosthenes to compute a logical
-! array of size n_max, where .true. in element i
-! indicates that i is a prime.
-! =====================================================
-    integer, intent(in)   :: n_max
-    logical, intent(out)  :: is_prime(n_max)
-    integer :: i
-    is_prime = .true.
-    is_prime(1) = .false.
-    do i = 2, int(sqrt(real(n_max)))
-        if (is_prime (i)) is_prime (i * i : n_max : i) = .false.
-    end do
-    return
-end subroutine
+PROGRAM Triangle
+    IMPLICIT NONE
+    REAL :: a, b, c, Area, arear, test
+
+    test = 1.45
     
-subroutine logical_to_integer(prime_numbers, is_prime, num_primes, n)
-! =====================================================
-! Translates the logical array from sieve to an array
-! of size num_primes of prime numbers.
-! =====================================================
-    integer                 :: i, j=0
-    integer, intent(in)     :: n
-    logical, intent(in)     :: is_prime(n)
-    integer, intent(in)     :: num_primes
-    integer, intent(out)    :: prime_numbers(num_primes)
-    do i = 1, size(is_prime)
-        if (is_prime(i)) then
-            j = j + 1
-            prime_numbers(j) = i
-        end if
-    end do
-end subroutine
+    PRINT *, 'Welcome, please enter the&
+                &lengths of the 3 sides.'
+    READ *, a, b, c
+    arear = Area(a,b,c)
+    PRINT *, 'Triangle''s area:  ', arear
+    PRINT *, 'Goodbye'
+END PROGRAM Triangle
+
+FUNCTION Area(x,y,z)
+    IMPLICIT NONE
+    REAL :: Area
+    REAL, INTENT( IN ) :: x, y, z
+    REAL :: theta, height
+    theta = ACOS((x**2+y**2-z**2)/(2.0*x*y))
+    height = x*SIN(theta)
+    Area = 0.5*y*height
+END FUNCTION Area
