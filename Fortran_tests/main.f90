b889ee06ce5371a4eb13604dd7603e6fe45eb0cb
Arturo Rodriguez <da.rodriguezh@uniandes.edu.co>
2021-03-17 21:08:19 -0500

    Prettier for pyhton file

--- Fortran_tests/main.f90
+++ Fortran_tests/main.f90
@@ -1,18 +1,15 @@
-!> Main Program for Video 4 of VSCode for Fortran
-!!
-!! @author Lukas Lamm
 program main
-    
+
     implicit none
-    
+
     real(kind=8), dimension(6) :: v
 
     v = (/1, 2, 3, 5, 6, 7/)
-    
-    ! Plot results to screen
-    write(*,'(a, f10.7)') 'Self-written routine: ', norm(v)
-    write(*,'(a, 3x, f10.7)') 'Intrinsic routine: ', norm2(v)
-    write(*,'(a, 10x, f10.7)') 'Difference: ', norm2(v) - norm(v)
+
+! Plot results to screen
+    write (*, '(a, f10.7)') 'Self-written routine: ', norm(v)
+    write (*, '(a, 3x, f10.7)') 'Intrinsic routine: ', norm2(v)
+    write (*, '(a, 10x, f10.7)') 'Difference: ', norm2(v) - norm(v)
 
 contains
 
@@ -24,7 +21,7 @@ contains
         integer                                :: i
 
         res = 0
-        do i = 1,size(vector),2
+        do i = 1, size(vector), 2
             res = res + vector(i)**2
         end do
 
--- Fortran_tests/test.f90
+++ Fortran_tests/test.f90
@@ -1,23 +1,28 @@
-PROGRAM Triangle
+program Triangle
     IMPLICIT NONE
-    REAL :: a, b, c, Area, arear, test
+    REAL :: a, b, c, arear, test
 
     test = 1.45
-    
+
     PRINT *, 'Welcome, please enter the&
                 &lengths of the 3 sides.'
     READ *, a, b, c
-    arear = Area(a,b,c)
+
+    arear = Area(a, b, c)
+
     PRINT *, 'Triangle''s area:  ', arear
     PRINT *, 'Goodbye'
-END PROGRAM Triangle
 
-FUNCTION Area(x,y,z)
-    IMPLICIT NONE
-    REAL :: Area
-    REAL, INTENT( IN ) :: x, y, z
-    REAL :: theta, height
-    theta = ACOS((x**2+y**2-z**2)/(2.0*x*y))
-    height = x*SIN(theta)
-    Area = 0.5*y*height
-END FUNCTION Area
+contains
+    function Area(x, y, z) result(res)
+        IMPLICIT NONE
+        REAL :: res
+        REAL, INTENT(IN) :: x, y, z
+        REAL :: theta, height
+        theta = ACOS((x**2 + y**2 - z**2)/(2.0*x*y))
+        height = x*SIN(
+        theta)
+        res = 0.5*y*height
+    end function Area
+
+end program Triangle
