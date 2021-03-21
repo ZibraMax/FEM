49f71c55ef12439728b65cb02b20cad3e8b9b5b1
Arturo Rodriguez <da.rodriguezh@uniandes.edu.co>
2021-03-21 15:41:23 -0500

    Cambios en readme

--- FEM/Mesh/delaunay.py
+++ FEM/Mesh/delaunay.py
@@ -39,39 +39,39 @@ class Delaunay(Geometry):
         Geometry.__init__(self, dictionary, gdls, tipos, nvn=nvn, segments=seg)
         self.mask = vertices
 
+    @staticmethod
+    def _strdelaunay(constrained=True, delaunay=True, a=None, q=None, o=1):
+        """Create a string for the delaunay triangulation constructor
 
-def _strdelaunay(constrained=True, delaunay=True, a=None, q=None, o=1):
-    """Create a string for the delaunay triangulation constructor
+        Args:
+                constrained (bool, optional): Makes the triangulation constrained. Defaults to True.
+                delaunay (bool, optional): Makes all triangles delaunay. Defaults to True.
+                a ([type], optional): Maximum area of triange. Defaults to None.
+                q ([type], optional): Minimum triangle angle <=35. Defaults to None.
+                o (int, optional): Order of element if 2, quadratic elements are generated. Defaults to 1.
 
-    Args:
-            constrained (bool, optional): Makes the triangulation constrained. Defaults to True.
-            delaunay (bool, optional): Makes all triangles delaunay. Defaults to True.
-            a ([type], optional): Maximum area of triange. Defaults to None.
-            q ([type], optional): Minimum triangle angle <=35. Defaults to None.
-            o (int, optional): Order of element if 2, quadratic elements are generated. Defaults to 1.
-
-    Returns:
-            str: A string containing the input parameters for the Delaunay1V constructor
-    """
-    p = ''
-    if o == 2:
-        o = '-o2'
-    else:
-        o = ''
-    if constrained:
-        p = 'p'
-    if a == None:
-        a = ''
-    else:
-        a = 'a'+format(a)
-    D = ''
-    if delaunay:
-        D = 'D'
-    if q == None:
-        q = ''
-    else:
-        if type(q) == int:
-            if q > 35:
-                raise "No se puede crear una triangulacion con angulos menores a 35 grados"
-        q = 'q'+format(q)
-    return p+a+D+q+'i'+o
+        Returns:
+                str: A string containing the input parameters for the Delaunay1V constructor
+        """
+        p = ''
+        if o == 2:
+            o = '-o2'
+        else:
+            o = ''
+        if constrained:
+            p = 'p'
+        if a == None:
+            a = ''
+        else:
+            a = 'a'+format(a)
+        D = ''
+        if delaunay:
+            D = 'D'
+        if q == None:
+            q = ''
+        else:
+            if type(q) == int:
+                if q > 35:
+                    raise "No se puede crear una triangulacion con angulos menores a 35 grados"
+            q = 'q'+format(q)
+        return p+a+D+q+'i'+o
--- Test/Test5.py
+++ Test/Test5.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 import FEM
 from FEM import Mesh
 
-a = 5
-b = 5
+a = 1
+b = 1
 E = 200000
 v = 0.2
-
-vertices = [[0,0],[a,0],[a,a],[0,a]]
-params = Mesh.Delaunay._strdelaunay(constrained=True,delaunay=True,a='0.0005',o=2)
-geometria = Mesh.Delaunay1V(vertices, params)
+G = E / (2 * (1 + v))
+phi = 1
+vertices = [[0, 0], [a, 0], [a, a], [0, a]]
+params = Mesh.Delaunay._strdelaunay(
+    constrained=True, delaunay=True, a='0.0005', o=2)
+geometria = Mesh.Delaunay(vertices, params)
 geometria.saveMesh('Mesh_tests/Square_torsion')
-O = FEM.Torsion2D(geometria,G,phi)
+O = FEM.Torsion2D(geometria, G, phi)
 O.solve()
 plt.show()
