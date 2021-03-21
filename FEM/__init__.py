6639b244e007a54d8bbcdc654ddb7273ba121b66
Arturo Rodriguez <da.rodriguezh@uniandes.edu.co>
2021-03-20 21:42:21 -0500

    Paquete publicado en versión 1.0.0

--- FEM/__init__.py
+++ FEM/__init__.py
@@ -11,5 +11,8 @@ from .PlaneStress import *
 from .PlaneStressNonLocal import *
 from .PlaneStrain import *
 
+__version__ = "1.0.0"
+__author__ = "Arturo Rodriguez - da.rodriguezh@uniandes.edu.co"
+
 __all__ = ["Elements", "Core", "Torsion2D", "EDO1D", "EulerBernoulliBeam",
            "PlaneStress", "PlaneStrain", "PlaneStressNonLocal", "Mesh"]
