95aabdd976a530921a5bcc87bd932504e0884385
Arturo <da.rodriguezh@uniandes.edu.co>
2022-01-22 22:32:56 -0500

    Minor changes

--- Examples/example37.py
+++ Examples/example37.py
@@ -1,3 +1,4 @@
+import triangle
 from FEM.Heat2D import Heat2D
 from FEM.Mesh.Delaunay import Delaunay
 from FEM.Utils.polygonal import roundCorner, giveCoordsCircle
@@ -16,9 +17,41 @@ vert_orig, seg_orig = giveCoordsCircle(cent, radi, n=150)
 
 vert, seg = giveCoordsCircle([0, 1], 1, n=50)
 holes = [{'center': [0, 1], 'segments': seg, 'vertices': vert}]
-params = Delaunay._strdelaunay(constrained=True, delaunay=True, a='0.03', o=2)
+params = Delaunay._strdelaunay(constrained=True, delaunay=True, a='0.03', o=1)
 geometria = Delaunay(vert_orig, params, 1, holes_dict=holes)
 geometria.setCbe(geometria.cbOnHole(0, t0, 1))
+
+nodes = geometria.gdls
+n = len(nodes)
+triangles = geometria.dictionary
+piramides = []
+m = 5
+h = 1.0
+
+dz = h/(m-1)
+
+dddnodes = np.zeros([m*n, 3])
+for i in range(m):
+    dddnodes[n*(i):n*(i+1), :2] = nodes
+    dddnodes[n*(i):n*(i+1), -1] = i*dz
+
+for i in range(m-1):
+    for t in triangles:
+        t = np.array(t)
+        nodossup = n*(i) + t
+        nodosinf = n*(i+1) + t
+        p = nodossup.tolist()+nodosinf.tolist()
+
+        piramides += [[p[2], p[5], p[0], p[1]]]
+        piramides += [[p[0], p[5], p[3], p[4]]]
+        piramides += [[p[0], p[5], p[4], p[1]]]
+
+dictionary = {"coords": dddnodes.tolist(), "prisms": piramides}
+
+with open('geometry.json', 'w') as f:
+    f.write(format(dictionary).replace("'", '"'))
+
+
 O = Heat2D(geometria, k, k)
 for i in range(len(seg_orig)):
     O.defineConvectiveBoderConditions(i, beta, Ta)
