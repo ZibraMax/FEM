7fbf04086fc44fe43ba7ffa7deb00ae6a086be78
Arturo <da.rodriguezh@uniandes.edu.co>
2022-02-19 12:59:07 -0500

    Pls

--- Examples/example9.py
+++ Examples/example9.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 from FEM.Geometry import Geometry2D
 from FEM.Elasticity2D import PlaneStressSparse
-from FEM.Utils.polygonal import
 
 E = 21000000.0  # MPa
 v = 0.2  # m
