2de12dbafa6035d62cc536353683cf5e7474d34f
Arturo <da.rodriguezh@uniandes.edu.co>
2022-02-20 18:48:19 -0500

    send help

--- Examples/example3.py
+++ Examples/example3.py
@@ -1,9 +1,76 @@
-if __name__ == '__main__':
-    #!/usr/bin/python
-    # -*- coding: utf-8 -*-
+"""
+********************************************
+Geometry creation using triangles. Torsion2D
+********************************************
+
+An square shape section when a unitary rotation is applied.
+
+The input mesh is created using the Delaunay class using second order elements.
+
+Geomery
+#######
+
+.. figure:: https://raw.githubusercontent.com/ZibraMax/FEM/master/Examples/examples_results/example3_geometry.png
+   :align: center
+
+   Input geometry created using Delaunay triangulations.
+
+Result
+######
+
+.. figure:: https://raw.githubusercontent.com/ZibraMax/FEM/master/Examples/examples_results/example3.png
+   :align: center
+
+   Analysis results.
+
+Code
+####
+
+.. code:: python
+
     import matplotlib.pyplot as plt
     from FEM.Torsion2D import Torsion2D
     from FEM.Geometry import Geometry2D, Delaunay
+
+    a = 1
+    b = 1
+    E = 200000
+    v = 0.27
+    G = E/(2*(1+v))*0+1
+    phi = 1
+
+    # Use these lines to create the input geometry and file
+    # coords = np.array([[0, 0], [1, 0], [1, 1], [0, 1.0]])
+    # params = Delaunay._strdelaunay(a=0.0001, o=2)
+    # geo = Delaunay(coords, params)
+    # geo.exportJSON('Examples/Mesh_tests/Square_torsion.json')
+
+    geometria = Geometry2D.importJSON(
+        'Examples/Mesh_tests/Square_torsion.json')
+    geometria.show()
+    plt.show()
+    O = Torsion2D(geometria, G, phi, verbose=True)
+    O.solve()
+    plt.show()
+
+"""
+
+if __name__ == '__main__':
+    try:
+        import matplotlib.pyplot as plt
+        from FEM.Torsion2D import Torsion2D
+        from FEM.Geometry import Geometry2D, Delaunay
+    except Exception as e:
+        import os
+        import inspect
+        import sys
+        currentdir = os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe())))
+        parentdir = os.path.dirname(currentdir)
+        sys.path.insert(0, parentdir)
+        import matplotlib.pyplot as plt
+        from FEM.Torsion2D import Torsion2D
+        from FEM.Geometry import Geometry2D, Delaunay
 
     a = 1
     b = 1
@@ -18,8 +85,10 @@ if __name__ == '__main__':
     # geo.exportJSON('Examples/Mesh_tests/Square_torsion.json')
     geometria = Geometry2D.importJSON(
         'Examples/Mesh_tests/Square_torsion.json')
-    # geometria.show()
-    # plt.show()
+    geometria.show()
+    plt.savefig('Examples/examples_results/example3_geometry.jpg')
+    plt.show()
     O = Torsion2D(geometria, G, phi, verbose=True)
     O.solve()
+    plt.savefig('Examples/examples_results/example3.jpg')
     plt.show()
---
+++ Examples/example4.py
@@ -0,0 +1,70 @@
+"""
+********************************
+Ordinary diferential equation 1D
+********************************
+
+The current file ust the general formulation of the EDO1D Class using custom function coeficients.
+
+Geomery
+#######
+
+.. math::
+
+    a(x)\\frac{d^2u}{dx^2}+c(x)u=f(x)
+    a(x)=x^2-2
+    c(x)=x-3
+    f(x)=6(x^2-2)+(x-3)(3x^2)
+
+    U|_0=0
+    U|_L=3
+    L=1
+
+Result
+######
+
+.. figure:: https://raw.githubusercontent.com/ZibraMax/FEM/master/Examples/examples_results/example4.png
+   :align: center
+
+   Analysis results.
+
+Code
+####
+
+.. code:: python
+
+
+"""
+
+
+if __name__ == '__main__':
+    try:
+        import matplotlib.pyplot as plt
+        from FEM.EDO1D import EDO1D
+        from FEM.Geometry import Lineal
+    except Exception as e:
+        import os
+        import inspect
+        import sys
+        currentdir = os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe())))
+        parentdir = os.path.dirname(currentdir)
+        sys.path.insert(0, parentdir)
+        import matplotlib.pyplot as plt
+        from FEM.EDO1D import EDO1D
+        from FEM.Geometry import Lineal
+
+    def a(x): return (x[0]**2-2)
+    def c(x): return (x[0]-3)
+    def f(x): return (x[0]**2-2)*6+(x[0]-3)*(3*x[0]**2)
+
+    cbe = [[0, 0], [-1, 3]]
+
+    lenght = 1
+    n = 500
+    o = 2
+    geometria = Lineal(lenght, n, o)
+    O = EDO1D(geometria, a, c, f)
+    O.cbe = cbe
+    O.solve()
+    plt.savefig('Examples/examples_results/example4.jpg')
+    plt.show()
