f1d9e499d0103901a48d3be8b5913315dd3e0f70
Arturo <da.rodriguezh@uniandes.edu.co>
2022-02-20 18:54:05 -0500

    a

--- Examples/example4.py
+++ Examples/example4.py
@@ -32,7 +32,26 @@ Code
 
 .. code:: python
 
+    import matplotlib.pyplot as plt
+    from FEM.EDO1D import EDO1D
+    from FEM.Geometry import Lineal
 
+    # Define functions of the diferential equation
+    def a(x): return (x[0]**2-2)
+    def c(x): return (x[0]-3)
+    def f(x): return (x[0]**2-2)*6+(x[0]-3)*(3*x[0]**2)
+
+    # Define border conditions. List of border conditions. In the first node, value=0.0, in the last node, value = 3.0
+    cbe = [[0, 0.0], [-1, 3.0]]
+
+    lenght = 1
+    n = 500
+    o = 2
+    geometria = Lineal(lenght, n, o)
+    O = EDO1D(geometria, a, c, f)
+    O.cbe = cbe
+    O.solve()
+    plt.show()
 """
 
 
@@ -57,7 +76,7 @@ if __name__ == '__main__':
     def c(x): return (x[0]-3)
     def f(x): return (x[0]**2-2)*6+(x[0]-3)*(3*x[0]**2)
 
-    cbe = [[0, 0], [-1, 3]]
+    cbe = [[0, 0.0], [-1, 3.0]]
 
     lenght = 1
     n = 500
