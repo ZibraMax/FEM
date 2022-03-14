48dc7905bf2970d7f670fbab546895202a0e5c6e
Arturo <da.rodriguezh@uniandes.edu.co>
2022-03-14 10:30:36 -0500

    Muchascosas uswwwsws

--- Examples/ExampleE.py
+++ Examples/ExampleE.py
@@ -8,42 +8,25 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from FEM.Elasticity3D import Elasticity
     from FEM.Geometry import Geometry3D, Delaunay
-
+    from FEM.Solvers.Lineal import LinealEigen
     E = 21000000.0
     v = 0.2
-    h = 0.6
-    b = 0.3
-    L = 2.5
+    h = 20
+    b = 20
+    L = 20
 
-    gamma = 23.54
+    gamma = 2.54
 
     coords = [[0.0, 0.0], [L, 0.0], [L, h], [0.0, h]]
-    params = Delaunay._strdelaunay(True, True, 0.01, 30, 1)
-    geo = Delaunay(coords, params, 1)
+    params = Delaunay._strdelaunay(True, True, 1, 30, 1)
+    geo = Delaunay(coords, params, 3)
     geo.show()
     plt.show()
 
-    base = Region2D(
-        np.array([[0.0, 0.0, 0.0], [0.0, h, 0.0], [0.0, h, b], [0.0, 0.0, b]]))
-    geometria = geo.extrude(b, 5, fast=True, regions=[base])
-    cb = geometria.cbFromRegion(0, 0.0, 1)
-    cb += geometria.cbFromRegion(0, 0.0, 2)
-    cb += geometria.cbFromRegion(0, 0.0, 3)
-    geometria.setCbe(cb)
+    geometria = geo.extrude(b, 5, fast=True)
 
     O = Elasticity(geometria, E, v, gamma/9.81,
-                   fy=lambda x: -gamma, verbose=True)
+                   verbose=True, solver=LinealEigen)
     O.solve()
-    print('A')
-    omh = scipy.sparse.linalg.spsolve(O.M, O.K)
-    print('A')
-    eiv, eigvec = scipy.sparse.linalg.eigs(omh, k=30, which='SR')
-    print('Open c++')
-    y = O.geometry.exportJSON()
-    pjson = json.loads(y)
-    pjson["disp_field"] = eigvec.real.T.tolist()
-    y = json.dumps(pjson)
-    with open("../FEM C++/docs/resources/exported3.json", "w") as f:
-        f.write(y)
-    # np.savetxt('a.csv', O.U, delimiter=',', fmt='%s')
+    O.exportJSON('noporfavor.json')
     # a = 0
