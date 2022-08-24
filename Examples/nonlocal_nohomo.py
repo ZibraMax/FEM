7a5f043d953f23573a16002970bd7342d06603d8
Arturo Rodriguez <da.rodriguezh@uniandes.edu.co>
2022-08-24 11:17:56 -0500

    No funcionan las integrales de gamma

---
+++ Examples/nonlocal_nohomo.py
@@ -0,0 +1,44 @@
+if __name__ == '__main__':
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from FEM.Elasticity2D import PlaneStressNonLocalSparseNonHomogeneous
+    from FEM.Geometry.Geometry import Geometry2D
+    from FEM.Utils import enmalladoFernando
+
+    E = 2.1*10**6
+    v = 0.2
+    u = 0.001
+    t = 0.5
+    l = 0.1
+    z1 = 0.5
+    LR = 6*l
+    P = 1
+    a = 5
+
+    nx = 100
+    ny = 100
+
+    coords, dicc = enmalladoFernando(a, a, nx, ny)
+
+    geometria = Geometry2D(dicc, coords, ['C2V']*len(dicc), 2, fast=True)
+
+    geometria.generateRegionFromCoords([0, 0], [a, 0])
+    geometria.generateRegionFromCoords([a, 0], [a, a])
+    geometria.generateRegionFromCoords([a, a], [0, a])
+    geometria.generateRegionFromCoords([0, a], [0, 0])
+    cb = geometria.cbFromRegion(3, 0, 1)
+    cb += geometria.cbFromRegion(3, 0, 2)
+    cb += geometria.cbFromRegion(1, u, 1)
+    geometria.setCbe(cb)
+
+    # geometria.show(draw_bc=True, label_bc=True)
+    # plt.show()
+
+    def af(rho):
+        return (0.5/np.pi/l/l/t)*np.exp(-rho)
+
+    O = PlaneStressNonLocalSparseNonHomogeneous(
+        geometria, E, v, t, l, z1, Lr=6*l, af=af, verbose=True)
+    O.solve()
+    O.exportJSON('NonLocalNonHomogeneous.json')
+    plt.show()
--- src/FEM/Elasticity2D.py
+++ src/FEM/Elasticity2D.py
@@ -813,3 +813,207 @@ class PlaneStressNonLocalSparse(PlaneStressSparse):
         if self.calculateMass:
             self.M = self.M.tocsr()
         logging.info('Done!')
+
+
+class PlaneStressNonLocalSparseNonHomogeneous(PlaneStressSparse):
+
+    def __init__(self, geometry: Geometry, E: Tuple[float, list], v: Tuple[float, list], t: Tuple[float, list], l: float, z1: float, Lr: float, af: Callable, rho: Tuple[float, list] = None, fx: Callable = lambda x: 0, fy: Callable = lambda x: 0, **kargs) -> None:
+
+        self.l = l
+        self.Lr = Lr
+        self.af = af
+        self.z1 = z1
+        self.z2 = 1.0-self.z1
+
+        PlaneStressSparse.__init__(
+            self, geometry, E, v, t, rho, fx, fy, **kargs)
+        self.properties['l'] = self.l
+        self.properties['Lr'] = self.Lr
+        self.properties['af'] = None
+        self.properties['z1'] = self.z1
+        self.properties['z2'] = self.z2
+        nonlocals = self.geometry.detectNonLocal(Lr)
+        for e, dno in zip(self.elements, nonlocals):
+            e.enl = dno
+        self.name = 'Plane Stress Isotropic non local sparse'
+
+        self.KL = sparse.lil_matrix((self.ngdl, self.ngdl))
+        self.KNL = sparse.lil_matrix((self.ngdl, self.ngdl))
+        if self.calculateMass:
+            self.M = sparse.lil_matrix((self.ngdl, self.ngdl))
+
+    def elementMatrices(self) -> None:
+        """Calculate the elements matrices
+        """
+        logging.info('Calculating gamma functions for all elements')
+        for e in tqdm(self.elements, unit='Local'):
+            e.gammas = []
+            for _xloc in e._x:
+                gamma = 0.0
+                for inl in e.enl:
+                    enl = self.elements[inl]
+                    for _xnloc, _wnloc, _detjacnloc in zip(enl._x, enl.W, enl.detjac):
+                        ro = np.linalg.norm(_xloc-_xnloc)/self.l
+                        gamma += _wnloc*_detjacnloc
+                e.gammas.append(gamma)
+            e.gammas = np.array(e.gammas)
+        for e in tqdm(range(len(self.elements)), unit='Local'):
+            self.elementMatrix(e)
+
+    def elementMatrix(self, ee: 'Element') -> None:
+        """Calculates a single element local and nonlocal matrices
+
+        Args:
+            ee (int): Element to be calculated
+        """
+        e = self.elements[ee]
+        m = len(e.gdl.T)
+
+        Fux = np.zeros([m, 1])
+        Fvx = np.zeros([m, 1])
+
+        _x, _p = e._x, e._p
+        detjac = e.detjac
+        dpx = e.dpx
+
+        c11 = self.C11[ee]
+        c12 = self.C12[ee]
+        c22 = self.C22[ee]
+        c66 = self.C66[ee]
+        C = np.array([
+            [c11, c12, 0.0],
+            [c12, c22, 0.0],
+            [0.0, 0.0, c66]])
+
+        Fe = np.zeros([2*m, 1])
+        Ke = np.zeros([2*m, 2*m])
+        if self.calculateMass:
+            Me = np.zeros([2*m, 2*m])
+
+        o = [0.0]*m
+        for k in range(len(e.Z)):  # Iterate over gauss points on domain
+            B = np.array([
+                [*dpx[k, 0, :], *o],
+                [*o, *dpx[k, 1, :]],
+                [*dpx[k, 1, :], *dpx[k, 0, :]]])
+
+            P = np.array([
+                [*_p[k], *o],
+                [*o, *_p[k]]])
+            Ke += self.t[ee]*(B.T@C@B)*detjac[k]*e.W[k]
+            if self.calculateMass:
+                Me += self.rho[ee]*self.t[ee]*(P.T@P)*detjac[k]*e.W[k]
+            Fe += self.t[ee]*(P.T@np.array([[self.fx(_x[k])],
+                                            [self.fy(_x[k])]]))*detjac[k]*e.W[k]
+
+        if e.intBorders:  # Cambiar esto a la notación matricial
+            for j in range(len(e.borders)):
+                border = e.borders[j]
+                if (len(border.properties['load_x']) + len(border.properties['load_y'])):
+                    _x, _p = e.T(e.Tj[j](border.Z.T))
+                    _s = border.TS(border.Z.T)
+                    detjac = border.coords[-1, 0]*0.5
+                    for i in range(m):
+                        for fx in border.properties['load_x']:
+                            for k in range(len(border.Z)):
+                                Fux[i, 0] += fx(_s[k, 0])*_p[k, i] * \
+                                    detjac*border.W[k]
+                        for fy in border.properties['load_y']:
+                            for k in range(len(border.Z)):
+                                Fvx[i, 0] += fy(_s[k, 0])*_p[k, i] * \
+                                    detjac*border.W[k]
+        subm = np.linspace(0, 2*m-1, 2*m).reshape([2, m]).astype(int)
+        Fe[np.ix_(subm[0])] += Fux
+        Fe[np.ix_(subm[1])] += Fvx
+        self.F[np.ix_(e.gdlm)] += Fe
+
+        self.KL[np.ix_(e.gdlm, e.gdlm)] += Ke
+
+        if self.calculateMass:
+            self.M[np.ix_(e.gdlm, e.gdlm)] += Me
+
+        # e.knls = []
+        for inl in tqdm(e.enl, unit=' Nolocal'):
+            enl = self.elements[inl]
+            mnl = len(enl.gdl.T)
+            onl = [0.0]*mnl
+            Knl = np.zeros([2*m, 2*mnl])
+            _xnl = enl._x
+            detjacnl = enl.detjac
+            dpxnl = enl.dpx
+
+            for k in range(len(e.Z)):
+                B = np.array([
+                    [*dpx[k, 0, :], *o],
+                    [*o, *dpx[k, 1, :]],
+                    [*dpx[k, 1, :], *dpx[k, 0, :]]])
+
+                for knl in range(len(enl.Z)):
+                    ro = np.linalg.norm(_x[k]-_xnl[knl])/self.l
+                    azn = self.af(ro)
+                    Bnl = np.array([
+                        [*dpxnl[knl, 0, :], *onl],
+                        [*onl, *dpxnl[knl, 1, :]],
+                        [*dpxnl[knl, 1, :], *dpxnl[knl, 0, :]]])
+
+                    Knl += self.t[ee]*self.t[inl]*azn*(Bnl.T@C@B)*detjac[k] * \
+                        e.W[k]*detjacnl[knl]*enl.W[knl]
+            # e.knls.append(Knl)
+            self.KNL[np.ix_(e.gdlm, enl.gdlm)] += Knl
+
+    def profile(self, p0: list, p1: list, n: float = 100) -> None:
+        """Generate a profile between selected points
+
+        Args:
+            p0 (list): start point of the profile [x0,y0]
+            p1 (list): end point of the profile [xf,yf]
+            n (int, optional): NUmber of samples for graph. Defaults to 100.
+
+        """
+
+        _x = np.linspace(p0[0], p1[0], n)
+        _y = np.linspace(p0[1], p1[1], n)
+        X = np.array([_x, _y])
+        U = []
+        U1 = []
+        U2 = []
+        U3 = []
+        _X = []
+        def dist(X): return np.sqrt((p0[0]-X[0])**2+(p0[1]-X[1])**2)
+        for i in range(n):
+            for _, e in enumerate(self.elements):
+                if e.isInside(X.T[i]):
+                    z = e.inverseMapping(np.array([X.T[i]]).T)
+                    _, u, du = e.giveSolutionPoint(z, True)
+                    U += [u.tolist()]
+                    U1 += (du[:, 0, 0]).tolist()
+                    U2 += (du[:, 1, 1]).tolist()
+                    U3 += (1/2*(du[:, 0, 1]+du[:, 1, 0])).tolist()
+                    _X.append(dist(X.T[i]))
+                    break
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 3, 1)
+        ax.plot(_X, U1, color='black')
+        ax.grid()
+        ax.set_xlabel('d')
+        ax.set_ylabel(r'$\varepsilon_{xx}$')
+        ax = fig.add_subplot(1, 3, 2)
+        ax.plot(_X, U2, color='black')
+        ax.grid()
+        ax.set_xlabel('d')
+        ax.set_ylabel(r'$\varepsilon_{yy}$')
+        ax = fig.add_subplot(1, 3, 3)
+        ax.plot(_X, U3, color='black')
+        ax.grid()
+        ax.set_xlabel('d')
+        ax.set_ylabel(r'$\varepsilon_{xy}$')
+        return _X, U1, U2, U3, U
+
+    def ensembling(self) -> None:
+        """Creation of the system sparse matrix. Force vector is ensembled in integration method
+        """
+        logging.info('Ensembling equation system...')
+        self.K = self.KL*self.z1 + self.KNL*self.z2
+        if self.calculateMass:
+            self.M = self.M.tocsr()
+        logging.info('Done!')
