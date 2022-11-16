3a31c9f1759609d038ca9a268f58743488994063
Arturo Rodriguez <davidarturo9911@hotmail.com>
2022-11-16 17:31:55 -0500

    No

--- Examples/Punto6.py
+++ Examples/Punto6.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    from FEM.Geometry.Geometry import Geometry
+    from FEM.Geometry.Geometry import Geometry2D as Geometry
     from FEM.Poisson2D import Poisson2D
     phi = 2
     nodos = [[0.0, 0.0], [0.5, 0.0], [1.0, 0.0],
@@ -12,9 +12,10 @@ if __name__ == '__main__':
     geometry = Geometry(elementos, nodos, tipos)
     geometry.cbe = [[2, 0], [5, 0], [7, 0], [8, 0], [0, 18], [3, 18]]
     geometry.cbn = [[6, 0], [1, 0]]
-    # geometry.show(draw_segs=True, draw_labels=True, draw_bc=True, label_bc=True)
-    # plt.show()
+    geometry.show(draw_segs=True, draw_labels=True,
+                  draw_bc=True, label_bc=True)
+    plt.show()
     O = Poisson2D(geometry, phi)
-    O.solve(plot=False)
+    O.solve(plot=True)
     # plt.show()
     plt.show()
