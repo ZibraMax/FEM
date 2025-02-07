b0a63aed65ef55fb7244bc0d8e7eb66f36b62bc0
Arturo Rodriguez <davidarturo9911@hotmail.com>
2025-02-06 19:45:01 -0600

    changes

--- src/FEM/Unhinged.py
+++ src/FEM/Unhinged.py
@@ -24,9 +24,33 @@ class Truss(Core):
         super().__init__(geometry, solver, **kwargs)
         self.name = 'Truss analysis (2D and 3D)'
         if isinstance(E, float) or isinstance(E, int):
-            E = [E]*len(geometry.elements)
+            E = np.array([E]*len(geometry.elements))
         if isinstance(A, float) or isinstance(A, int):
-            A = [A]*len(geometry.elements)
+            A = np.array([A]*len(geometry.elements))
 
         self.properties["E"] = E
         self.properties["A"] = A
+        self.ngdl = len(self.geometry.elements)
+        self.A = sparse.lil_matrix((self.ngdl, self.ngdl))
+        self.G = np.zeros(self.ngdl)
+
+    def elementMatrices(self):
+        """Calculate element stiffness matrices and assemble global stiffness matrix.
+        """
+        for i, e in enumerate(self.geometry.elements):
+            # Element length and angle
+            x1, y1 = self.geometry.nodes[e[0]]
+            x2, y2 = self.geometry.nodes[e[1]]
+            L = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+            c = (x2 - x1) / L
+            s = (y2 - y1) / L
+
+            # Element stiffness matrix
+            k = self.properties["E"][i] * self.properties["A"][i] / L
+            k_matrix = k * np.array([[c**2, c*s, -c**2, -c*s],
+                                     [c*s, s**2, -c*s, -s**2],
+                                     [-c**2, -c*s, c**2, c*s],
+                                     [-c*s, -s**2, c*s, s**2]])
+
+            # Assemble global stiffness matrix
+            self.A[i:i+4, i:i+4] += k_matrix
