de5eac719b111d48e131f56acacacf7e509a46ec
Arturo Rodriguez <davidarturo9911@hotmail.com>
2025-07-22 19:43:25 -0500

    yweas

--- runner.py
+++ runner.py
@@ -3,17 +3,18 @@ import pandas as pd
 
 data_file = './runs.csv'
 df = pd.read_csv(data_file)
-ELES = df['l'].unique()
-
 
 print('=' * 50)
-for l in ELES:
-    ERES = ','.join(df[df['l'] == l]['R'].unique().astype('str'))
-    print("Running for l =", l, "with R =", ERES)
-    nex = 30
-    E = 30e6
-    v = 0.3
-    rho = 1
-    fa = 1
+for row in df.iloc:
+    R = row['R']
+    l = row['l']
+    nex = int(row['nex'])
+    E = row['E']
+    v = row['v']
+    rho = row['rho']
+    af = row['af']
+
+    print("Running with parameters:")
+    print(f"R: {R}, l: {l}, nex: {nex}, E: {E}, v: {v}, rho: {rho}, af: {af}")
     proc = Popen(
-        f'C:/Users/ar257/Desktop/FEM/.venv/Scripts/python.exe srunner2.py {ERES} {l} {nex} {E} {v} {rho} {fa}', creationflags=CREATE_NEW_CONSOLE)
+        f'C:/Users/ar257/Desktop/FEM/.venv/Scripts/python.exe nolocal_runner_beams.py {R} {l} {nex} {E} {v} {rho} {af}', creationflags=CREATE_NEW_CONSOLE)
