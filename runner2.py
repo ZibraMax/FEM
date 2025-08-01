c11fdeb8548c97b63b48ed3d3ea414cbf3f23774
Arturo Rodriguez <davidarturo9911@hotmail.com>
2025-08-01 16:33:35 -0500

    More data

--- runner2.py
+++ runner2.py
@@ -9,7 +9,7 @@ l = 1
 L = 20
 
 print('=' * 50)
-for nex in [2, 4, 6, 8, 10, 12, 14, 16]:
+for nex in [10, 12, 14, 16]:
     print(f'Running for nex = {nex}')
     proc = Popen(
-        f'C:/Users/david/Desktop/FEM/.venv/Scripts/python.exe nolocal_runner.py {L} {l} {nex} {C11} {C12} {C44} {rho}', creationflags=CREATE_NEW_CONSOLE)
+        f'C:/Users/ar257/Desktop/FEM/.venv/Scripts/python.exe nolocal_runner.py {L} {l} {nex} {C11} {C12} {C44} {rho}', creationflags=CREATE_NEW_CONSOLE)
